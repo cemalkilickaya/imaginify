aa5e5ac936b98f713bdb6d65ad46c6fb90ca621a
cemalkilickaya <43484779+cemalkilickaya@users.noreply.github.com>
2024-03-02 15:13:28 +0300

    fix

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -210,7 +210,7 @@ const TransformationForm = ({
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        {creditBalance < Math.abs(creditFee) && <InsufficientCreditsModal />}
+        {/* {creditBalance < Math.abs(creditFee) && <InsufficientCreditsModal />} */}
         <CustomField
           control={form.control}
           name="title"
